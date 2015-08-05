9cdfe54ce0c8ed0af22b503b6b948ee3e0f9fd55
The Pffy Authors <pffy@users.noreply.github.com>
2015-08-05 00:53:43 -0700

    initial release

---
+++ sample/sampletxt.js
@@ -0,0 +1,38 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+/**
+ * name     : sampletxt.js
+ * version  : 1
+ * updated  : 2015-08-03
+ * license  : http://unlicense.org/ The Unlicense
+ * git      : https://github.com/pffy/chrome-app-chinesefrequency
+ *
+ */
+
+
+const SAMPLE_TXT = "提到意大利，大家一定會想到意大利粉（Pasta）。意大利旅遊局曾經對遊客做過一項調查，諮詢意大利的象徵是甚麼，結果令人感到十分意外，因為答案既不是古羅馬鬥獸場，亦不是比薩斜塔，而是意大利粉，可見意粉與意大利人的密切關係。每個意大利人每年要吃20多公斤意大利粉，當地每年生產320多萬噸的乾麵條，其中一半以上是出口。\n\n麵條要有硬感\n\n意粉是意大利人的主食，最初的意大利粉是沒有下醬料的，大家都是用手抓着吃。後來，貴族們開始使用餐具，淋上醬汁，慢慢醬汁裏面又加上各種不同的調味品、香料、肉類、海鮮、香腸、蔬菜、水果等。\n\n在意大利，最完美的意粉煮法，是每根麵條的中心都有很細的硬感，他們有個專門的稱呼「aldente」。而意粉太熟是真的沒有口感，故此，煮意粉也要有技巧，不能用慢火浸熟。火候要夠大，時間要算得準，要過冷河，停止其繼續煮熟或黏在一起。\n\n乾品易保存\n\n除了意大利粉，意式麵食還有很多種類，據說有563種，形狀色素都不同，名字也很有趣，有螺旋粉、通心粉、煲呔粉、管型粉、蝴蝶粉、小耳朵粉等等。除了形狀千差萬別，意大利粉還分為乾粉和新鮮粉。這兩種的用法不一樣，新鮮粉的雞蛋含量比乾粉高，比較適合做有餡的麵食，其保養期短，必須冷藏。乾麵則可儲存在乾燥的地方，至少可以保存一年。因此，很多人習慣食用乾麵條，不少廚師都認為乾麵條的口感比新鮮麵條好。\n\nFit型Sun知\n\n資歷：畢業於美國加州柏克萊大學營養學系，曾在美國任職多年註冊營養師";
